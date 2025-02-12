9ff3f15b996d75e7074bf4ee5ccb5cad62bfe567
Dan White <dan.white@osucascades.edu>
2025-02-11 23:15:43 -0800

    updated assignment5-2 with complete logic

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
 scene.setBackgroundColor(6)
 scene.setTileMapLevel(assets.tilemap`level`)
 
-//console.log(assets.tilemap`level`.getTileImage(0).width)
+// create a new type to store our enemy. It will also store the direction
+type Enemy = {
+    sprite: Sprite,
+    direction: String
+}
+
+// create an array to store our enemies
+let enemies: Enemy[] = []
+
+// constant values
+const PLAYERBUFFER = 60
+const FOODBUFFER = 20
+const FOODSPAWN = 500
+const ENEMYSPAWN = 3000
 
 // get the width of the entire map dynamically based on the size of the tiles and the number of tiles
 let tileWidth = assets.tilemap`level`.getTileImage(0).width
@@ -9,21 +22,14 @@ let tileHeight = assets.tilemap`level`.getTileImage(0).height
 let totalWidth = assets.tilemap`level`.width * tileWidth
 let totalHeight = assets.tilemap`level`.height * tileHeight
 
-
 // Create player sprite, set up movement and set the camera to follow the sprite
 let playerSprite = sprites.create(assets.animation`playerFront`[0], SpriteKind.Player)
 controller.moveSprite(playerSprite, 80 , 80)
 scene.cameraFollowSprite(playerSprite)
-
-info.setScore(0)
-
 // this is the initial direction of the player
 let direction = "None"
 
-const PLAYERBUFFER = 60
-const FOODBUFFER = 20
-const FOODSPAWN = 500
-const ENEMYSPAWN = 3000
+info.setScore(0)
 
 // store the food images in an array
 let foodImages = [
@@ -34,21 +40,17 @@ let foodImages = [
     assets.image`ham`
 ]
 
-
-// this is how fast the food will spawn
-
-
 // This runs every foodSpawn number of milleseconds
 game.onUpdateInterval(FOODSPAWN, () => {
     // call the spawn food function to randomly spawn food
     spawnFood()
 })
 
+// This runs every enemySpawn number of milleseconds
 game.onUpdateInterval(ENEMYSPAWN, () => {
     // call the spawn food function to randomly spawn food
     spawnEnemy()
 })
-
 
 game.onUpdate(() => {
     // Code in this function will run once per frame. MakeCode
@@ -83,14 +85,12 @@ game.onUpdate(() => {
         playerSprite.setImage(assets.animation`playerFront`[0])
     }
 
-    // let enemySprites = sprites.allOfKind(SpriteKind.Enemy)
-    // for (let i = 0; i < enemySprites.length; i++){
-    //     enemySprites[i]
-        
-    //     animateEnemy(enemySprites[i])
-
-    // }
-
+    for (let i = 0; i < enemies.length; i++){
+        if (enemies[i].direction != getEnemyDirection(enemies[i])) {
+            enemies[i].direction = getEnemyDirection(enemies[i])
+            animateEnemy(enemies[i])
+        }
+    }
 });
 
 function spawnEnemy() {
@@ -102,7 +102,7 @@ function spawnEnemy() {
     let randY
     while (!valid) {
         valid = true
-        // choose a random position anywher on the map
+        // choose a random position anywhere on the map
         randX = randint(xOffset, totalWidth - xOffset)
         randY = randint(yOffset, totalHeight - yOffset)
 
@@ -112,24 +112,41 @@ function spawnEnemy() {
         }
     }
 
-
-
+    // create an anemy sprite, set the position to a random position, and set it to follow the player
     let enemySprite = sprites.create(assets.animation`enemyLeft`[0], SpriteKind.Enemy)
     enemySprite.setPosition(randX, randY)
     enemySprite.follow(playerSprite, 60, 50)
 
-    animateEnemy(enemySprite)
+    // store our enemySprite in a new Enemy type
+    let enemy: Enemy = {sprite: enemySprite, direction: ""}
+
+    // set the enemy direciton and animate the enemy
+    enemy.direction = getEnemyDirection(enemy)
+    animateEnemy(enemy)
+
+    // add the enemy to our list of enemies so we can access it later
+    enemies.push(enemy)
 
 
 }
 
-function animateEnemy(enemySprite: Sprite) {
-    if (enemySprite.x < playerSprite.x) {
-        // face right
-        animation.runImageAnimation(enemySprite, assets.animation`enemyRight`, 200, true)
+// this will get direction the enemy should be facing based on it's position relative to the player
+function getEnemyDirection(enemy: Enemy) {
+    if (enemy.sprite.x < playerSprite.x) {
+        return "Right"
     }
     else {
-        animation.runImageAnimation(enemySprite, assets.animation`enemyLeft`, 200, true)
+        return "Left"
+    }
+}
+
+// this function will animate the enemy based on the direction it is facing (using enemy.direction)
+function animateEnemy(enemy: Enemy) {
+    if (enemy.direction == "Right") {
+        animation.runImageAnimation(enemy.sprite, assets.animation`enemyRight`, 200, true)
+    }
+    else {
+        animation.runImageAnimation(enemy.sprite, assets.animation`enemyLeft`, 200, true)
     }
 }
 
@@ -137,8 +154,6 @@ function spawnFood() {
     // choose a random integer between 0 and 1 less than the length of the food images
     let r = randint(0, foodImages.length - 1)
     
-    
-
     // Set the xOffset and yOffset so the food doesn't spawn in the walls
     let xOffset = tileWidth + foodImages[r].width
     let yOffset = tileHeight + foodImages[r].height
@@ -172,8 +187,6 @@ function spawnFood() {
                 valid = false
             }
         }
-
-
     }
 
     // create a sprite using a random image selected from the foodImages array
